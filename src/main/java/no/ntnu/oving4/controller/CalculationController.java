5542ab9bd5ef2383b3a5f60a6c8b9f6a8367a1ee
Thadshajini Paramsothy <thadshap@stud.ntnu.no>
2022-03-04 08:59:40 +0100

    oving 4 backend done

--- src/main/java/no/ntnu/oving4/controller/CalculationController.java
+++ src/main/java/no/ntnu/oving4/controller/CalculationController.java
@@ -3,20 +3,25 @@ import no.ntnu.oving4.dto.CalculationDto;
 import no.ntnu.oving4.model.Calculation;
 import no.ntnu.oving4.repo.CalculationRepo;
 import no.ntnu.oving4.service.CalculationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api")
-@CrossOrigin("http://localhost:8081/")
+@CrossOrigin("http://localhost:8082/")
 public class CalculationController {
     @Autowired
     private CalculationRepo calculationRepo;
     @Autowired
     private CalculationService calculationService;
+
+    Logger logger = LoggerFactory.getLogger(CalculationController.class);
 
     @GetMapping("/calculations")
     public List<Calculation> fetchCalculation(){
@@ -40,8 +45,10 @@ public class CalculationController {
 
         newExpression.setEquation(mathExpression.getEquation());
         newExpression.setAnswer(Double.parseDouble(answer));
+        logger.info("The answer has been calculated and saved in the attribute 'answer'=" + newExpression.getAnswer());
 
         calculationRepo.save(newExpression);
+        logger.info("An expression has been saved in the mock database");
         return newExpression;
     }
 }
